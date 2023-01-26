7661202858a9961dfc8a4ff28fcb7a5492d8e657
fufikcfg <cool.isaak2016@yandex.ru>
2023-01-26 18:17:59 +0500

    update

--- palindrom.cs
+++ palindrom.cs
@@ -8,15 +8,24 @@
 
         if (num_letters < 2) { Console.Write("Количество букв < 2"); }
 
-        for (int j = 1; j <= num_letters; j++)
+        for (int j = 0; j <= num_letters; j++)
         {
             int letter_num = rand.Next(0, letters.Length - 1);
             word += letters[letter_num];
         }
         word = word.Remove(num_letters2);
+
         for (int k = num_letters3 - 1; k >= 0; k--)
         {
             word += word[k];
+        }
+        if (num_letters % 2 == 0)
+        {
+            word = word.Remove(word.Length / 2 - 1, word.Length / 2 + 1);
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                word += word[i];
+            }
         }
         Console.WriteLine(word);
     }
